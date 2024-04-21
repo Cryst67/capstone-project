cdd46ae01afe2e2d05872aeeca70ef97d47c790f
Aryan Swaminath <aryan.swaminath@aol.com>
2024-04-21 15:47:01 -0500

    added superearth

--- components/screens/Map.tsx
+++ components/screens/Map.tsx
@@ -1,11 +1,24 @@
-import { View, Text, Image, ScrollView } from 'react-native';
+import { View, Image, ScrollView } from 'react-native';
 
 import { useTheme } from '../../contexts/ThemeContext';
 import colors from '../../constants/colors';
+import Svg, { Image as SvgImage } from 'react-native-svg';
 
 const Map = () => {
   const { theme } = useTheme();
   const activeColors = colors[theme.mode];
+
+  const imageWidth = 350;
+  const imageHeight = 350;
+
+  // Calculate the center of the image
+  const centerX = imageWidth / 2;
+  const centerY = imageHeight / 2;
+
+  // Adjust these values based on your data's coordinate system
+  const pointSize = 40;
+  const pointX = centerX - pointSize / 2; // Subtract half the size of the point image for centering
+  const pointY = centerY - pointSize / 2;
 
   return (
     <ScrollView
@@ -16,17 +29,36 @@ const Map = () => {
       }}
       contentContainerStyle={{ alignItems: 'center', justifyContent: 'center' }}
     >
-      {theme.mode === 'dark' ? (
-        <Image
-          source={require('../../assets/warmap_dark.png')}
-          style={{ width: 350, height: 350 }}
-        />
-      ) : (
-        <Image
-          source={require('../../assets/warmap_light.png')}
-          style={{ width: 350, height: 350 }}
-        />
-      )}
+      <View>
+        {theme.mode === 'dark' ? (
+          <Image
+            source={require('../../assets/warmap_dark.png')}
+            style={{ width: 350, height: 350 }}
+          />
+        ) : (
+          <Image
+            source={require('../../assets/warmap_light.png')}
+            style={{ width: 350, height: 350 }}
+          />
+        )}
+        <Svg
+          height='350'
+          width='350'
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}
+        >
+          <SvgImage
+            width={40}
+            height={40}
+            href={require('../../assets/superearth.png')}
+            x={pointX}
+            y={pointY}
+          />
+        </Svg>
+      </View>
     </ScrollView>
   );
 };
