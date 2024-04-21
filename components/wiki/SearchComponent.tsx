91ccb3b537b797e6f23997740e97b4feb7f1cd76
Grant T Nelson <twincracker@gmail.com>
2024-04-21 17:04:24 -0500

    added better styling

--- components/wiki/SearchComponent.tsx
+++ components/wiki/SearchComponent.tsx
@@ -2,57 +2,77 @@ import React, { useState } from 'react';
 import { View, TextInput, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { useTheme } from '../../contexts/ThemeContext';
 import colors from '../../constants/colors';
-import { StratagemEntry } from '../../api/data/stratagems'; // Import the interface
+
+interface StratagemEntry {
+  name: string;
+  content: string;
+  cost?: number;
+  sequence: string[];
+}
 
 interface SearchProps {
   data: Record<string, StratagemEntry>;
 }
 
 const SearchComponent: React.FC<SearchProps> = ({ data }) => {
-    const { theme } = useTheme();
-    const [searchTerm, setSearchTerm] = useState('');
-  
-    const filteredData = searchTerm.trim() ? Object.values(data).filter((entry) =>
-      entry.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ) : [];
-  
-    // Dynamic theme-dependent styles
-    const dynamicStyles = StyleSheet.create({
-      itemContainer: {
-        backgroundColor: colors[theme.mode].itemBackground,
-        padding: 20,
-        marginVertical: 8,
-        borderRadius: 5,
-      },
-      itemText: {
-        color: colors[theme.mode].textColor,
-        fontSize: 16,
-      }
-    });
-  
-    return (
-      <View style={{ flex: 1, backgroundColor: colors[theme.mode].backgroundColor }}>
-        <TextInput
-          style={styles.input}
-          placeholder="Search..."
-          onChangeText={setSearchTerm}
-          value={searchTerm}
-        />
-        {searchTerm && (
-          <FlatList
-            data={filteredData}
-            keyExtractor={(item) => item.name}
-            renderItem={({ item }) => (
-              <TouchableOpacity style={dynamicStyles.itemContainer}>
-                <Text style={dynamicStyles.itemText}>{item.name}</Text>
-                <Text style={dynamicStyles.itemText}>{item.content.replace(/<\/?p>/g, '').trim()}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        )}
-      </View>
-    );
+  const { theme } = useTheme();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [activeName, setActiveName] = useState<string | null>(null);
+
+  const filteredData = searchTerm.trim() ? Object.values(data).filter((entry) =>
+    entry.name.toLowerCase().includes(searchTerm.toLowerCase())
+  ) : [];
+
+  const dynamicStyles = StyleSheet.create({
+    itemContainer: {
+      backgroundColor: colors[theme.mode].itemBackground,
+      padding: 20,
+      marginVertical: 8,
+      borderRadius: 5,
+      borderColor: theme.mode === 'dark' ? 'white' : 'black',
+      borderWidth: activeName ? 1 : 0,
+    },
+    itemText: {
+      color: colors[theme.mode].textColor,
+      fontSize: 16,
+    }
+  });
+
+  const handlePress = (name: string) => {
+    setActiveName(name === activeName ? null : name);
   };
+
+  const renderItem = ({ item }: { item: StratagemEntry }) => (
+    <TouchableOpacity style={dynamicStyles.itemContainer} onPress={() => handlePress(item.name)}>
+      <Text style={dynamicStyles.itemText}>{item.name}</Text>
+      {activeName === item.name && (
+        <>
+          <Text style={dynamicStyles.itemText}>Cost: {item.cost}</Text>
+          <Text style={dynamicStyles.itemText}>Sequence: {item.sequence.join(', ')}</Text>
+          <Text style={dynamicStyles.itemText}>{item.content.replace(/<\/?p>/g, '')}</Text>
+        </>
+      )}
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={{ flex: 1, backgroundColor: colors[theme.mode].backgroundColor }}>
+      <TextInput
+        style={styles.input}
+        placeholder="Search..."
+        onChangeText={setSearchTerm}
+        value={searchTerm}
+      />
+      {searchTerm && (
+        <FlatList
+          data={filteredData}
+          keyExtractor={(item) => item.name}
+          renderItem={renderItem}
+        />
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   input: {
